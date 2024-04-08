5cbcf1a6e43776d4db953b227332b9401a9701d1
Andy Wang <ywang980@mtroyal.ca>
2024-04-08 13:03:22 -0600

    Cleaned up App and main.

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,14 @@
 import React, { useState, useEffect } from 'react';
-
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 
 import LoginForm from './components/LoginForm/LoginForm';
 import RegisterForm from './components/RegisterForm/RegisterForm';
 import ForgotPasswordForm from './components/ForgotPasswordForm/ForgotPasswordForm';
+
 import Home from './components/Home';
-import CircuitDetails from './components/CircuitDetailsPopup/CircuitDetails';
-import { FavoritesProvider } from './components/ContextProviders/FavoritesProvider';
-import ContextProviderWrapper from './components/ContextProviders/ContextProviderWrapper'
-import DriverDetails from './components/DriverDetailsPopup/DriverDetails';
-import ConstructorDetails from './components/ConstructorDetailsPopup/ConstructorDetails';
 //IMPORTANT NEED TO USE REACT-ROUTER-DOM-VERSION-5
 
 const App = () => {
-  /*
-  return (
-    <ContextProviderWrapper>
-      <HeaderBar />
-      <HomeViewContent />
-    </ContextProviderWrapper>
-  );
-  */
   return (
     <Router>
       <Switch>
@@ -36,11 +23,6 @@ const App = () => {
         </Route>
         <Route path='/Home'>
           <Home />
-        </Route>
-        <Route path='/test'>
-          <ContextProviderWrapper>
-         <  ConstructorDetails  Name = "Hamilton" nationality="English" url ="Englishmen.com" />
-          </ContextProviderWrapper>
         </Route>
       </Switch>
     </Router>
