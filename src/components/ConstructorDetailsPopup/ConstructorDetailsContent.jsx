fc6db1004655f4000a1d09f0a81285dc87d1a4c1
NB00001100 <100167954+NB00001100@users.noreply.github.com>
2024-04-08 00:05:47 -0600

    changed sum css

--- src/components/ConstructorDetailsPopup/ConstructorDetailsContent.jsx
+++ src/components/ConstructorDetailsPopup/ConstructorDetailsContent.jsx
@@ -5,9 +5,9 @@ const ConstructorDetailsContent = ({ Name,nationality, url }) => {
 
   return (
     <>
-      <p style={{ marginLeft: '5px' }}><span>Name: </span>{Name}</p>
-      <p style={{ marginLeft: '5px' }}><span>Nationality : </span>{nationality}</p>
-      <p style={{ marginLeft: '5px' }}><span>Website: </span><a href={url}>{url}</a></p>
+      <p style={{ marginLeft: '5px', color: 'white' }}><span>Name: </span>{Name}</p>
+      <p style={{ marginLeft: '5px', color: 'white' }}><span>Nationality : </span>{nationality}</p>
+      <p style={{ marginLeft: '5px', color: 'white' }}><span>Website: </span><a href={url}>{url}</a></p>
       <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', gap: '40px' }}>
         <div style={{ width: '500px', height: '500px', border: '1px solid black', padding: '10px' }}>
        
--- src/components/DriversDetailsPopup/DriversDetailsContent.jsx
+++ src/components/DriversDetailsPopup/DriversDetailsContent.jsx
@@ -5,11 +5,11 @@ const DriversDetailsContent = ({ Name,dob,age,nationality, url }) => {
 
   return (
     <>
-      <p style={{ marginLeft: '5px' }}><span>Name: </span>{Name}</p>
-      <p style={{ marginLeft: '5px' }}><span>Date of Birth: </span>{dob}</p>
-      <p style={{ marginLeft: '5px' }}><span>Age: </span>{age}</p>
-      <p style={{ marginLeft: '5px' }}><span>Nationality : </span>{nationality}</p>
-      <p style={{ marginLeft: '5px' }}><span>Website: </span><a href={url}>{url}</a></p>
+      <p style={{ marginLeft: '5px', color: 'white' }}><span>Name: </span>{Name}</p>
+      <p style={{ marginLeft: '5px', color: 'white'}}><span>Date of Birth: </span>{dob}</p>
+      <p style={{ marginLeft: '5px', color: 'white' }}><span>Age: </span>{age}</p>
+      <p style={{ marginLeft: '5px', color: 'white' }}><span>Nationality : </span>{nationality}</p>
+      <p style={{ marginLeft: '5px', color: 'white' }}><span>Website: </span><a href={url}>{url}</a></p>
       <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', gap: '40px' }}>
         <div style={{ width: '500px', height: '500px', border: '1px solid black', padding: '10px' }}>
        
